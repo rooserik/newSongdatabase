8212c1b11d3d8cad41a3a65c46c95c6ca4b052ab
rooserik <rooserik@gmail.com>
2016-02-17 18:10:46 +0100

    added tests for controller and add show views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,4 +11,32 @@ class ArtistsController < ApplicationController
   def edit
     @artist=Artist.find(params[:id])
   end
+
+  def update
+    @artist=Artist.find(params[:id])
+    if @artist=Artist.update(artist_params)
+      redirect_to @artist, notice: "artist succesfully edited"
+    else
+      render :edit, notice:"error, try again"
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artist_path
+  end
+
+  def create
+    if @artist=Artist.create(artist_params)
+      redirect_to @artist, notice: "artist succesfully created"
+    else
+      render :new, notice: "error, try again"
+    end
+  end
+
+private
+  def artist_params
+  params.require(:artist).permit(:name,:rec_company,:genre,:poster)
+  end
 end
--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ArtistsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+      @artist = artists(:one)
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:artists)
+    end
+
+    test "should get new" do
+      get :new
+      assert_response :success
+    end
+
+    test "should create artist" do
+        assert_difference('Artist.count') do
+          artist :create, artist: { name: @artist.content, genre: @artist.genre, rec_company: @artist.rec_company }
+        end
+
+        assert_redirected_to artist_path(assigns(:artist))
+      end
+
+      # test "should show post" do
+      #   get :show, id: @post
+      #   assert_response :success
+      # end
+      #
+      # test "should get edit" do
+      #   get :edit, id: @post
+      #   assert_response :success
+      # end
 end
